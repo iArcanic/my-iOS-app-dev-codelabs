d8251e9efa13c7665ee4200ff6556df1ff29b0c1
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-15 16:55:39 +0100

    Initial commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Preetham Ananthkumar on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
