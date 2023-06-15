aaa6e87b819513ce84bf06281b5fcdd36389209c
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-15 17:24:58 +0100

    Changed text size and alignment

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -12,12 +12,15 @@ struct MeetingView: View {
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.tophalf.fill")
                 }
-                VStack {
+                Spacer()
+                VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
