56f38d0eda70a6ba26a89e4dc718816ff1933c17
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-15 18:13:12 +0100

    Stylized the card view

--- Scrumdinger/Scrumdinger/CardView.swift
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -18,8 +18,11 @@ struct CardView: View {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
                 Spacer()
                 Label("\(scrum.lengthinMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
             }
+            .font(.caption)
         }
+        .padding()
     }
 }
 
