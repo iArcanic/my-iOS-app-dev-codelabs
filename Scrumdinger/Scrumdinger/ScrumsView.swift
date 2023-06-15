6368777ceb54a432da28ce72c2996622adb34b3d
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-15 18:46:51 +0100

    Added a view with list of all sample data scrum meetings

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Preetham Ananthkumar on 15/06/2023.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
