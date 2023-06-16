2a068284f67a898edc1fe1f1b6e5d4cc798a23c5
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-16 15:29:36 +0100

    Added navigation to MeetingView from DetailView. Added title for each scrum meeting in DetailView

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -13,9 +13,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Start meeting", systemImage: "timer")
-                    .font(.headline)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start meeting", systemImage: "timer")
+                        .font(.headline)
                     .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -39,6 +41,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
