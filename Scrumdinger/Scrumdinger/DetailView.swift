6928a9f5e5b13b86c05e39b34161e8fda8920f98
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-16 15:14:33 +0100

    Added text stylizing for theme label in DetailView

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -22,6 +22,16 @@ struct DetailView: View {
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
                 .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
             }
         }
     }
