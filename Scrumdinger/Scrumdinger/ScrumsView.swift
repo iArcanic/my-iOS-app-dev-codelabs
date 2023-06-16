b5adbf20c2682bfbf4bcbb93538a2be3fb3cd33e
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-16 14:52:28 +0100

    Added toolbar with "new Scrum" plus button

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -19,10 +19,16 @@ struct ScrumsView: View {
                 .listRowBackground(scrum.theme.mainColor)
             }
             .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
-
+    
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         ScrumsView(scrums: DailyScrum.sampleData)
