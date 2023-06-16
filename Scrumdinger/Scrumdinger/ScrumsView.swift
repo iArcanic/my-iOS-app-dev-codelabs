8f32a2b18d09ceb72558cec66f57fa3c9efe6016
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-16 12:43:30 +0100

    Added navigation for each CardView scrum

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -11,9 +11,14 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack() {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
         }
     }
 }
