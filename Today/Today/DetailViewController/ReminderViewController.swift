7c7ecca7afba4e94b7742a5589908cf39db0ca13
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-28 15:10:24 +0100

    Added an emumeration for reminder task rows

--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -23,4 +23,13 @@ class ReminderViewController: UICollectionViewController {
     required init?(coder: NSCoder) {
         fatalError("Always initialise ReminderViewController using init(reminder:)")
     }
+    
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        case .title: return reminder.title
+        }
+    }
 }
