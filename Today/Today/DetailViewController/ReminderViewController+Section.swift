6b23066fb906ee2509e08886aa41751fe6a3b0bb
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-06-30 17:56:47 +0100

    Created sections for editing mode

---
+++ Today/Today/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,26 @@
+//
+//  ReminderViewController+Section.swift
+//  Today
+//
+//  Created by Preetham Ananthkumar on 30/06/2023.
+//
+
+import Foundation
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+        
+        var name: String {
+            switch self {
+            case .view: return ""
+            case .title: return NSLocalizedString("Title", comment: "Title section name")
+            case .date: return NSLocalizedString("Date", comment: "Date section name")
+            case .notes: return NSLocalizedString("Notes", comment: "Notes section name")
+            }
+        }
+    }
+}
--- Today/Today/DetailViewController/ReminderViewController.swift
+++ Today/Today/DetailViewController/ReminderViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ReminderViewController: UICollectionViewController {
-    private typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
-    private typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Row>
+    private typealias DataSource = UICollectionViewDiffableDataSource<Section, Row>
+    private typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Row>
     
     var reminder: Reminder
     private var dataSource: DataSource!
@@ -67,8 +67,17 @@ class ReminderViewController: UICollectionViewController {
     
     private func updateSnapshot() {
         var snapshot = Snapshot()
-        snapshot.appendSections([0])
-        snapshot.appendItems([Row.title, Row.date, Row.notes], toSection: 0)
+        snapshot.appendSections([.view])
+        snapshot.appendItems([Row.title, Row.date, Row.notes], toSection: .view)
         dataSource.apply(snapshot)
+    }
+    
+    private func section(for indexPath: IndexPath) -> Section {
+        let sectionNumber = isEditing ? indexPath.section + 1 : indexPath.section
+        
+        guard let section = Section(rawValue: sectionNumber) else {
+            fatalError("Unable to find matching version")
+        }
+        return section
     }
 }
